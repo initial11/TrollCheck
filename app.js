1cd8d39c4becd727db083b037d5f01a954321d55
initial <68012725+initial11@users.noreply.github.com>
2025-01-04 18:29:53 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+var app = new Framework7({
+  el: "#app", 
+	theme: "ios",	
+  name: 'TrollCheck',
+	id: 'com.Outlander.TrollCheck',
+	serviceWorker: {
+		path: "./service-worker.js"
+	}
+});
+
+var mainView = app.views.create('.view-main');
+function toggleDarkMode() {
+  
+    document.querySelector("html").classList.toggle("dark");
+}
+
+function applyDarkModeSetting() {
+    const htmlElement = document.querySelector("html");
+    const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const applyDarkMode = (e) => {
+        if (e.matches) {
+            htmlElement.classList.add("dark");
+        } else {
+            htmlElement.classList.remove("dark");
+        }
+    };
+
+    darkModeQuery.addListener(applyDarkMode);
+    applyDarkMode(darkModeQuery);
+}
+
+applyDarkModeSetting();
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistration().then(registration => {
+        if (!registration) {
+            navigator.serviceWorker.register("service-worker.js").then(() => {}).catch(() => {});
+        }
+    });
+	}
